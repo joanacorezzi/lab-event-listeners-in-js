6962d442f64e727277a6402e2ed517d7a4d6a507
Joana Corezzi Williams <joanacorezzi@gmail.com>
2025-10-21 16:30:52 -0500

    Lab event listeners in js completed

--- index.js
+++ index.js
@@ -3,18 +3,22 @@
 // Function to change the background color when a button is clicked
 function changeBackgroundColor() {
   // Implement the function to change background color
+  document.body.style.backgroundColor  = 'rgb(30, 35, 90)'; 
 }
 
 // Function to reset the background color when the body is double-clicked
 function resetBackgroundColor() {
   // Implement the function to reset background color
+  document.body.style.backgroundColor = '';
 }
-
 // Capture Keyboard Input
 
 // Function to display the key pressed by the user
 function displayKeyPress(event) {
   // Implement the function to display key pressed
+  const key = event.key.toUpperCase();
+  document.getElementById('keyPressDisplay').textContent = `Key pressed: ${key}`;
+  
 }
 
 // Process Text Input
@@ -22,6 +26,8 @@ function displayKeyPress(event) {
 // Function to display user input in real-time
 function displayUserInput() {
   // Implement the function to display user input
+  const inputText = document.getElementById('textInput').value;
+  document.getElementById('textInputDisplay').textContent = `You typed: ${inputText}`;
 }
 
 // Attach Event Listeners
